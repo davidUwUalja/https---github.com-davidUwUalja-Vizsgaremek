a6e6e3f9580725eb45c957bfc0325ac51c08a6b7
davidUwUalja <davidnorthkorodi@gmail.com>
2024-11-25 11:07:29 +0100

    asasdas

---
+++ script.js
@@ -0,0 +1,7 @@
+function elso() {
+    a = parseInt(document.getElementById("kezfogok").value);
+
+    let lehetoseg = (a * (a - 1)) / 2;
+
+    document.getElementById("elso").innerHTML = "asdasd " + lehetoseg;
+}
